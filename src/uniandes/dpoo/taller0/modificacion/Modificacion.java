d4d7ec21225190bd8c6cc7a88cc1a9d506dc0636
Guillermo Hernández <g.hernandeze@uniandes.edu.co>
2023-08-19 18:09:21 -0500

    Commit modificacion 1

--- src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -1,15 +1,7 @@
 package uniandes.dpoo.taller0.modificacion;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
-import uniandes.dpoo.taller0.modelo.Atleta;
-import uniandes.dpoo.taller0.modelo.Genero;
+import java.io.IOException;
 import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
 import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
 
@@ -22,12 +14,11 @@ public class Modificacion {
 		System.out.println("Hola, mundo!");
 	
 	
-	
 	try {
-        // Carga el archivo utilizando LoaderOlimpicos
+        // Carga el archivo 
         CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
         
-        // Imprime el país con más medallistas utilizando la instancia de CalculadoraEstadisticas
+        // Imprime el país con más medallistas 
         System.out.println(calc.paisConMasMedallistas());
     } catch (IOException e) {
         System.err.println("Error al cargar el archivo: " + e.getMessage());
